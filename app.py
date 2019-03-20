c39a5d61ebbb333d3b571b64509b4a1ea3f84eda
Blake Lockley <bwlockley@gmail.com>
2019-03-21 00:39:44 +1100

    Added example with apex charts

--- app.py
+++ app.py
@@ -17,5 +17,14 @@ def message():
     return jsonify({'message': 'Hello from the API!'})
 
 
+@app.route('/api/data')
+def data():
+
+    # Simulate a latency on the network
+    time.sleep(1)
+
+    return jsonify([70,55,35,50,49,60,70,91,125])
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,6 @@
-$('#request-data-button').click(function(){
+$('#request-data-button').click(function () {
     get_message();
+    load_chart();
 
     // The same behaviour can be observed without using async/await by relying on callbacks.
     // get_message_noawait();
@@ -18,8 +19,30 @@ async function get_message() {
 }
 
 function get_message_noawait() {
-    fetch('/api/message').then(res => res.json()).then(function(json) {
+    fetch('/api/message').then(res => res.json()).then(function (json) {
         const message = json['message'];
         $('#message').text(message);
     });
+}
+
+async function load_chart() {
+
+    const data = await fetch('/api/data').then(res => res.json());
+
+    var options = {
+        chart: {
+            type: 'bar'
+        },
+        series: [{
+            name: 'sales',
+            data: data
+        }],
+        xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+        }
+    }
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+    chart.render();
 }
